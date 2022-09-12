6273f6fcb02345b813784f03a9cdfef8680a764c
heyewuyue <jasonhejiahao2001@126.com>
2022-09-12 12:01:18 +0800

    前端基本实现

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 # DSL
 
 - 北京邮电大学计算机学院大三上程序设计实践课程大作业
-- client采用react，server采用fastapi
+
+## 前端
+
+- 前端采用React+Ant Design实现，部分参考[yehuozhili/chatbot-antd](https://github.com/yehuozhili/chatbot-antd)
+
+## 后端
+
+- 后端采用FastAPI实现。
--- client/src/DSLBot.jsx
+++ client/src/DSLBot.jsx
@@ -1,30 +1,24 @@
 import React from "react";
-import { List, Button, Modal, Input, Avatar} from "antd"
+import { List, Button, Modal, Input } from "antd"
 import axios from "axios"
 import { CustomerServiceOutlined, EnterOutlined } from "@ant-design/icons";
-import { RobotItem } from "./listItem";
+import { RobotItem } from "chatbot-antd/dist/listItem"
 import "antd/dist/antd.min.css";
 export default class DSLBot extends React.Component {
-    data = [
-        {
-            title: "aaa",
-        },
-        {
-            title: "bbb",
-        },
-        {
-            title: "ccc",
-        },
-    ]
     constructor(props) {
         super(props);
         this.state = {
-            modalOpen: false,
-            msgList: [{isUser: false, content: "欢迎使用DSL机器人客服"}],
-            inputValue: ""
+            modalOpen: false,  // 客服机器人窗口是否打开
+            msgList: [{isUser: false, content: "欢迎使用DSL机器人客服"}],  // 呈现在聊天框里的消息列表
+            inputValue: ""  // 当前输入框中的内容
         };
     }
 
+    // 处理按下回车键发送消息之后的函数
+    // 1. 将输入框清空
+    // 2. 将用户消息添加到消息列表
+    // 3. 将用户消息发送到服务端请求回复
+    // 4. 得到非空回复后，将回复消息添加到消息列表
     handleEnter = async () => {
         let inputBox = document.getElementById("inputBox");
         if (!inputBox.value)
@@ -40,7 +34,7 @@ export default class DSLBot extends React.Component {
             inputValue: ""
         });
 
-        setTimeout(() => {
+        setTimeout(() => {  // 设置500ms的延迟，让机器人回复自然一些
             if (retStr.data) {
                 this.setState({
                     msgList: [
@@ -53,6 +47,7 @@ export default class DSLBot extends React.Component {
         console.log(this.state.msgList);
     }
 
+    // 处理用户输入，更新inputValue
     keyUp = (event) => {
         this.setState({
             inputValue: event.target.value
@@ -69,9 +64,12 @@ export default class DSLBot extends React.Component {
 					top: "40%",
 				}}
 			>
+                {/* 显示在网页上的按钮 */}
 				<Button type="primary" onClick={() => this.setState({modalOpen: true})}>
 					<CustomerServiceOutlined></CustomerServiceOutlined>
 				</Button>
+
+                {/* 客服窗口 */}
                 <Modal open={this.state.modalOpen}
                     onCancel = {() => this.setState({modalOpen: false})}
                     footer = {
@@ -83,14 +81,19 @@ export default class DSLBot extends React.Component {
                     }
                     title = "DSL机器人"
                 >
-                    {/* TODO:  右对齐，滚动条*/}
                     <List id="msgBox"
                         split={false}
                         dataSource={this.state.msgList}
                         itemLayout="horizontal"
+                        style={{
+                            height: 400,
+                            overflow: 'scroll',
+                        }}
                         renderItem={item => (
-                            <List.Item >
-                                <RobotItem  isUser={item.isUser} text={item.content}></RobotItem>
+                            <List.Item style={item.isUser?
+                            {justifyContent:"flex-end", padding: "12px 10px"}:
+                            {justifyContent:"flex-start", padding: "12px 10px"}}>
+                                    <RobotItem isUser={item.isUser} text={item.content}></RobotItem>
                             </List.Item>
                         )}>
                     </List>
